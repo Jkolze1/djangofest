8338a1588a0100a1a65a10b73c4a797a3d9ebbb3
Jacqueline Kolze <jkolze1@stumail.jccc.edu>
2020-10-07 19:36:34 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server    
+const express = require('express');
+
+const path = require('path');   
+
+const app = express();   
+
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
